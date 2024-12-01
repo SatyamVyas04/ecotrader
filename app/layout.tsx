74f02254aad66fa91d322535fb8b020f8e90ac84
Satyam Vyas <vyas.satyam.04@gmail.com>
2024-12-01 17:25:37 +0530

    Update metadata in layout.tsx to enhance project description and add Open Graph data for improved SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,8 @@ const SpaceGrotesk = Space_Grotesk({
 
 export const metadata: Metadata = {
 	title: "Ecotrader",
-	description: "Welcome to Ecotrader",
+	description:
+		"Ecotrader empowers small investors to participate in the renewable energy market. Invest in solar, wind, hydrogen, and waste-to-energy projects while earning passive income and fighting climate change.",
 	icons: [
 		{
 			rel: "icon",
@@ -17,6 +18,14 @@ export const metadata: Metadata = {
 			url: "/ECOnew.png",
 		},
 	],
+	metadataBase: new URL("https://www.ecotrader.io"), // Replace with actual domain
+	openGraph: {
+		title: "Ecotrader: Revolutionizing Renewable Energy Investments",
+		description:
+			"Join the green energy revolution. Invest in sustainable projects and earn passive income while making a positive impact on the planet.",
+		type: "website",
+		url: "/",
+	},
 };
 
 export default function RootLayout({
